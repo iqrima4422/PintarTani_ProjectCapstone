c4a2e53612348af3baceb8dd0058d16b70c4c420
John Doe <johndoe@email.com>
2023-05-28 17:53:58 +0700

    Menambahkan View header, layout, footer untuk Admin

---
+++ resources/views/AdminView/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022</strong> <a>TOKO SEMBAKO BAROKAH | Online Shop </a>
+    <div class="float-right d-none d-sm-inline-block">
+      <a>All rights reserved.</a> 
+  </footer>
+
+  
