363f02e15a02d444588a97bcbf289d9727cc5e5b
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2023-06-08 23:05:05 +0700

    Fix route logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -27,6 +27,6 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        return redirect()->route('LoginPage');
+        return redirect()->route('welcome');
     }
 }
