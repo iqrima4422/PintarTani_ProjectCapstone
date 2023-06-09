fa3fbfd28fa770b09ab219341521261fd541ff60
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2023-06-09 15:32:30 +0700

    Fix route beranda PintarTani

--- resources/views/HomePage/navbar.blade.php
+++ resources/views/HomePage/navbar.blade.php
@@ -7,7 +7,7 @@
 
  <!-- Navbar Start -->
  <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-     <a href="#" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+     <a href="{{ route('HomePage') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
          <h1 class="m-0">Pintar Tani</h1>
      </a>
      <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
