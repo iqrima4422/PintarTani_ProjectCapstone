937fb1ae3a3e7579e94f198db48b8617cf11ce21
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2023-06-11 21:04:02 +0700

    Menambahkan suplierController

--- app/Http/Controllers/SuplierController.php
+++ app/Http/Controllers/SuplierController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Supplier;
+use PDF;
 
 class SuplierController extends Controller
 {
-    //
+    function create () {
+        return view('AdminView.DataSuplier.create')->with('title', 'Create Supplier');
+    }
+
+    function store (Request $request) {
+        $this->validate($request, [
+            'nama' => 'required',
+            'alamat' => 'required',
+            'telp' => 'required|unique:suppliers',
+        ]);
+
+        $suppliers = new Supplier;
+        $suppliers->nama = $request->nama;
+        $suppliers->alamat = $request->alamat;
+        $suppliers->telp = $request->telp;
+        $suppliers->save();
+
+        return redirect('/dataSupplier');
+    }
+
+    function edit ($id) {
+        $suppliers = Supplier::find($id);
+        return view('AdminView.DataSuplier.updatesuplier')->with('title', 'Edit Supplier')->with('suppliers', $suppliers);
+    }
+
+    function updateDataSupplier (Request $request, $id) {
+        $this->validate($request, [
+            'nama' => 'required',
+            'alamat' => 'required',
+            'telp' => 'required|unique:suppliers,telp,'.$id,
+        ]);
+
+        $suppliers = Supplier::find($id);
+        $suppliers->nama = $request->nama;
+        $suppliers->alamat = $request->alamat;
+        $suppliers->telp = $request->telp;
+        $suppliers->save();
+
+        return redirect('/dataSupplier');
+    }
+
+    function destroy ($id) {
+        $suppliers = Supplier::find($id);
+        $suppliers->delete();
+        return redirect('/dataSupplier');
+    }
+
+    function cetakDataSupplier()
+    {
+        $datasupplier = Supplier::all();
+        $pdf = PDF::loadView('AdminView.DataSuplier.cetakDataSupplier',['datasupplier' => $datasupplier]);
+        return $pdf->download('Data Supplier.pdf');
+    }
 }
