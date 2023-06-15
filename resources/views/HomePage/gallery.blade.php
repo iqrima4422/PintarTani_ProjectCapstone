9424833e53aaaf636c86707739b8ea32f00d7c5c
iqrima4422 <iqrimannisa@gmail.com>
2023-06-16 00:35:25 +0700

    Memperbaiki view Gallery & Navbar

--- resources/views/HomePage/gallery.blade.php
+++ resources/views/HomePage/gallery.blade.php
@@ -3,7 +3,7 @@
 <!-- Content Search Section Begin-->
 
 <!-- Page Header Start -->
-<div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+<div class="container-fluid page-header py-3 mb-4 wow fadeIn" data-wow-delay="0.1s">
     <div class="container text-center py-5">
         <h1 class="display-3 text-white mb-4 animated slideInDown">Produk Penjualan</h1>
         <nav aria-label="breadcrumb animated slideInDown">
@@ -14,74 +14,77 @@
         </nav>
     </div>
 </div>
-<!-- Page Header End -->
 
-<div class="row mb-2">
-    <div class="col-md-4 mb-2">
-        <form action="{{ route('SearchProduct') }}" method="GET">
-            <div class="input-group">
-                <input type="search" name="cari" class="form-control rounded" id="searchproduct"
-                    placeholder="Cari Barang Disini" aria-label="Search" aria-describedby="search-addon"
-                    value="{{old('cari')}}" />
-                <button type="submit" class="btn btn-outline-primary ml-2">Cari</button>
+<section>
+    <!-- Section tambahan di sini -->
+    <div class="container">
+        <div class="row mb-4">
+            <div class="col-md-4 mb-2">
+                <form action="{{ route('SearchProduct') }}" method="GET">
+                    <div class="input-group">
+                        <input type="search" name="cari" class="form-control rounded" id="searchproduct"
+                            placeholder="Cari Barang Disini" aria-label="Search" aria-describedby="search-addon"
+                            value="{{old('cari')}}" />
+                        <button type="submit" class="btn btn-outline-primary ml-2">Cari</button>
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-    <div class="col-lg-4 col-md-5">
-        <div class="filter__sort">
-            <form action="{{ route('Sorting') }}" method="get">
-                <div class="input-group">
-                    <span class="input-group-text">Sortir berdasarkan</span>
-                    <select name="sorting" class="form-select">
-                        <option value="product">Nama Produk</option>
-                        <option value="kategori">Kategori</option>
-                    </select>
-                    <div class="input-group-append">
-                        <button type="submit" class="btn btn-outline-primary">Sortir</button>
+            <div class="col-lg-4 col-md-5">
+                <div class="filter__sort">
+                    <form action="{{ route('Sorting') }}" method="get">
+                        <div class="input-group">
+                            <span class="input-group-text">Sortir berdasarkan</span>
+                            <select name="sorting" class="form-select">
+                                <option value="product">Nama Produk</option>
+                                <option value="kategori">Kategori</option>
+                            </select>
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-outline-primary">Sortir</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 py-2 mb-2">
+                <div class="filter__found">
+                    <h6><span>{{$total}}</span> Produk ditemukan </h6>
+                </div>
+            </div>
+        </div>
+
+        <!-- Main content -->
+        <div class="row">
+            @foreach ($galeri as $g)
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="featured__item">
+                    <div class="d-flex justify-content-center">
+                        <img class="img-fluid" src="{{'storage/'.$g->gambar}}">
+                    </div>
+                    <div class="featured__item__text">
+                        <div class="product__discount__item__text">
+                            <span>{{$g ->kategori}}</span>
+                            <h6><a href="#">{{$g ->product}}</a></h6>
+                            <span>{{$g ->merk}}</span>
+                            <h5>{{$g ->harga}}</h5>
+                        </div>
+                        <p class="btn-holder"><a href=" {{ route('AddCart', $g->id) }}"
+                                class="btn btn-warning btn-block text-center" role="button">Masukkan Keranjang</a> </p>
                     </div>
                 </div>
-            </form>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-4">
-        <div class="filter__found">
-            <h6><span>{{$total}}</span> Produk ditemukan </h6>
-        </div>
-    </div>
-</div>
-
-<!-- Main content -->
-<div class="row">
-    @foreach ($galeri as $g)
-    <div class="col-lg-3 col-md-4 col-sm-6">
-        <div class="featured__item">
-            <div class="d-flex justify-content-center">
-                <img class="img-fluid" src="{{'storage/'.$g->gambar}}">
             </div>
-            <div class="featured__item__text">
-                <div class="product__discount__item__text">
-                    <span>{{$g ->kategori}}</span>
-                    <h6><a href="#">{{$g ->product}}</a></h6>
-                    <span>{{$g ->merk}}</span>
-                    <h5>{{$g ->harga}}</h5>
-                </div>
-                <p class="btn-holder"><a href=" {{ route('AddCart', $g->id) }}"
-                        class="btn btn-warning btn-block text-center" role="button">Masukkan Keranjang</a> </p>
+            @endforeach
+        </div>
+
+        <div class="container">
+            @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
             </div>
+            @endif
+            <!-- @yield('content') -->
         </div>
     </div>
-    @endforeach
-</div>
-
-<div class="container">
-    @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
-    <!-- @yield('content') -->
-</div>
-</div>
 </section>
+
 @endsection
--- resources/views/HomePage/navbar.blade.php
+++ resources/views/HomePage/navbar.blade.php
@@ -1,82 +1,84 @@
- <!-- Spinner Start -->
- <div id="spinner"
-     class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-     <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
- </div>
- <!-- Spinner End -->
+<!-- Spinner Start -->
+<div id="spinner"
+    class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
+</div>
+<!-- Spinner End -->
 
- <!-- Navbar Start -->
- <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-     <a href="{{ route('HomePage') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-         <h1 class="m-0">Pintar Tani</h1>
-     </a>
-     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-         <span class="navbar-toggler-icon"></span>
-     </button>
-     <div class="collapse navbar-collapse" id="navbarCollapse">
-         <div class="navbar-nav ms-auto p-4 p-lg-0">
-             <a href="{{ route('HomePage') }}" class="nav-item nav-link">Beranda</a>
-             <a href="{{ route('GalleryPage') }}" class="nav-item nav-link">Produk</a>
-             <div class="nav-item dropdown">
-                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pembelian</a>
-                 <div class="dropdown-menu bg-light m-0">
-                     <a href="{{ route('PurchasePage') }}" class="dropdown-item">Riwayat Pembelian</a>
-                 </div>
-             </div>
-             <a href="{{ route('ArticlePage') }}" class="nav-item nav-link">Artikel</a>
-             <a href="{{ route('ContactPage') }}" class="nav-item nav-link">Kontak </a>
-             <div class="nav-item dropdown">
-                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Akun</a>
-                 <div class="dropdown-menu bg-light m-0">
-                     <a href="{{ route('ProfilePage') }}" class="dropdown-item">Profile</a>
-                     <a href="{{ route('logout') }}" class="dropdown-item">Logout</a>
-                 </div>
-             </div>
-             <div class="nav-item dropdown">
-                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                     <i class="fa fa-shopping-cart" aria-hidden="true"></i> Keranjang
-                 </a>
-                 <div class="dropdown-menu bg-light m-0">
-                     <div class="dropdown-item">
-                         <div class="col-lg-6 col-sm-6 col-6">
-                             <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                             <span class="badge badge-pill badge-danger bg-dark">
-                                 {{ count((array) session('cart'))}} Produk
-                             </span>
-                         </div>
-                         @php $total1 = 0 @endphp
-                         @foreach((array) session('cart') as $id => $details)
-                         @php $total1 += $details['harga'] * $details['quantity'] @endphp
-                         @endforeach
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+    <a href="{{ route('HomePage') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <h1 class="m-0">Pintar Tani</h1>
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="{{ route('HomePage') }}" class="nav-item nav-link">Beranda</a>
+            <a href="{{ route('GalleryPage') }}" class="nav-item nav-link">Produk</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pembelian</a>
+                <div class="dropdown-menu bg-light m-0">
+                    <a href="{{ route('PurchasePage') }}" class="dropdown-item">Riwayat Pembelian</a>
+                </div>
+            </div>
+            <a href="{{ route('ArticlePage') }}" class="nav-item nav-link">Artikel</a>
+            <a href="{{ route('ContactPage') }}" class="nav-item nav-link">Kontak</a>
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button">
+                    <i class="fa fa-shopping-cart" aria-hidden="true"></i> Keranjang
+                </a>
+                <div class="dropdown-menu  bg-light m-0">
+                    <div class="dropdown-item">
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-6 col-6">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                <span class="badge badge-pill badge-danger bg-dark">
+                                    {{ count((array) session('cart')) }} Produk
+                                </span>
+                            </div>
 
-                         <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
-                             <p>Total: <span class="">Rp. {{ $total1 }}</span></p>
-                         </div>
-                     </div>
-                     @if(session('cart'))
-                     @foreach(session('cart') as $id => $details)
-                     <div class="row cart-detail">
-                         <div class="col-lg-4 col-sm-4 col-4">
-                             <img src="{{'storage/'. $details['gambar'] }}" width="65" height="65" />
-                         </div>
-                         <div class="col-lg-6 col-sm-6 col-6 cart-detail-product">
-                             <p>{{ $details['product'] }}</p>
-                             <span class="price "> Rp. {{ $details['harga'] }}</span> <br>
-                             <span class="count"> Quantity:{{ $details['quantity'] }}</span>
-                         </div>
-                     </div>
-                     @endforeach
-                     @endif
+                        </div>
+                        <div class="col-lg-4 col-sm-4 col-4 text-right">
+                            @php $total1 = 0 @endphp
+                            @foreach((array) session('cart') as $id => $details)
+                            @php $total1 += $details['harga'] * $details['quantity'] @endphp
+                            @endforeach
+                            <p>Total: <span class="">Rp. {{ $total1 }}</span></p>
+                        </div>
+                    </div>
+                    @if(session('cart'))
+                    @foreach(session('cart') as $id => $details)
+                    <div class="dropdown-item">
+                        <div class="row">
+                            <div class="col-lg-4 col-sm-4 col-4">
+                                <img src="{{ 'storage/'. $details['gambar'] }}" width="50" height="50" />
+                            </div>
+                            <div class="col-lg-7 col-sm-7 col-7">
+                                <p>{{ $details['product'] }}</p>
+                                <span class="price">Rp. {{ $details['harga'] }}</span><br>
+                                <span class="count">Quantity:{{ $details['quantity'] }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                    @endif
+                    <div class="dropdown-item bg-primary btn-block">
+                        <a href="{{ route('Cart') }}" class="btn btn-primary">Tampilkan semua</a>
+                    </div>
+                </div>
 
-                     <div class="">
-                         <!-- <div class="text-center checkout"> -->
-                         <a href="{{ route('Cart') }}" class="btn btn-primary btn-block">Tampilkan semua</a>
-                         <!-- </div> -->
-                     </div>
-                 </div>
-             </div>
-         </div>
-         <!-- Notifications Dropdown Menu. -->
-     </div>
- </nav>
- <!-- Navbar End -->
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Akun</a>
+                <div class="dropdown-menu bg-light m-0">
+                    <a href="{{ route('ProfilePage') }}" class="dropdown-item">Profile</a>
+                    <a href="{{ route('logout') }}" class="dropdown-item">Logout</a>
+                </div>
+            </div>
+        </div>
+        <!-- Notifications Dropdown Menu. -->
+    </div>
+</nav>
+<!-- Navbar End -->
