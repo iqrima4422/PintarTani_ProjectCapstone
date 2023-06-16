4fa17e531b2ead3d11d7225eb8f0bc62df7eb164
iqrima4422 <iqrimannisa@gmail.com>
2023-06-16 11:57:34 +0700

    Menampilkan icon pada sidebar

--- resources/views/AdminView/sidebar.blade.php
+++ resources/views/AdminView/sidebar.blade.php
@@ -11,7 +11,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('storage/'.auth()->user()->foto) }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('img/admin/admin.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="{{ route('ProfilePageAdmin') }}" class="d-block">{{ auth()->user()->name }}</a>
@@ -34,73 +34,73 @@
         <nav class="mt-2" id="sidebar-menu">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+       with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{ route('HomePageAdmin') }} "
-                        id="data-pengguna" class="nav-link {{ ($title === "Home Page Admin") ? 'active' : ''}}">
-                        <i class="bi bi-people-fill"></i>
+                    <a href="{{ route('HomePageAdmin') }}" id="data-pengguna"
+                        class="nav-link {{ ($title === "Home Page Admin") ? 'active' : ''}}">
+                        <i class="bi-people-fill"></i>
+                        <p>Data Pengguna</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('ProfilePageAdmin') }}" id="profile"
+                        class="nav-link {{ ($title === "Profile Page") ? 'active' : ''}}">
+                        <i class="bi bi-person-badge"></i>
+                        <p>Profile</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" id="database-toko" class="nav-link">
+                        <i class="bi bi-clipboard2-data"></i>
                         <p>
-                            Data Pengguna
+                            Database Toko
+                            <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
-                </li>
-                <li class="nav-item">
-          <a href="{{ route('ProfilePageAdmin') }} " id="profile" class="nav-link {{ ($title === "Profile Page") ? 'active' : ''}}">
-          <i class="bi bi-person-badge"></i>
-              <p>Profile</p>
-            </a>
-          </li>
 
-                <li class="nav-item">
-            <a href="#" id="database-toko" class="nav-link">
-            <i class="bi bi-clipboard2-data"></i>
-              <p>
-              Database Toko
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
+                    <ul class="nav nav-treeview">
+                        <li id="data-product" class="nav-item">
+                            <a href="{{ route('DataProductPage') }}"
+                                class="nav-link {{ ($title === "Data Product") ? 'active' : ''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Data Produk</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('DataPenjualanPage') }}"
+                                class="nav-link {{ ($title === "Data Penjualan") ? 'active' : ''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Data Penjualan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('DataSupplierPage') }}"
+                                class="nav-link {{ ($title === "Data Supplier") ? 'active' : ''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Data Supplier</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('DataArticlePage') }}"
+                                class="nav-link {{ ($title === "Data Article") ? 'active' : ''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Data Article</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
 
-            <ul class="nav nav-treeview">
-              <li id="data-product" class="nav-item">
-                <a href="{{ route('DataProductPage') }}" class="nav-link
-                {{ ($title === "Data Product") ? 'active' : ''}}"">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Data Produk</p>
-                </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('DataPenjualanPage') }}"
-                        class="nav-link {{ ($title === "Data Penjualan") ? 'active' : ''}}"">
-                  <i class=" far fa-circle nav-icon"></i>
-                        <p> Data Penjualan</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('DataSupplierPage') }}"
-                        class="nav-link {{ ($title === "Data Supplier") ? 'active' : ''}}"">
-                  <i class=" far fa-circle nav-icon"></i>
-                        <p>Data Suplier</p>
-                    </a>
-                </li>
-                 <li class="nav-item">
-                    <a href="{{ route('DataArticlePage') }}"
-                        class="nav-link {{ ($title === "Data Article") ? 'active' : ''}}"">
-                  <i class=" far fa-circle nav-icon"></i>
-                        <p>Data Article</p>
+                <li class="nav-item bg-danger">
+                    <a href="{{ route('logout') }}" id="logout" class="nav-link">
+                        <!-- icon logout -->
+                        <i class="bi bi-box-arrow-left"></i>
+                        <p>Logout</p>
                     </a>
                 </li>
             </ul>
-            </li>
-
-            <li class="nav-item bg-danger">
-                <a href="{{ route('logout') }}" id="logout" class="nav-link">
-                    <i class="bi bi-box-arrow-in-left"></i>
-                    <p>Logout</p>
-                </a>
-            </li>
-            </ul>
-
         </nav>
+
         <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
