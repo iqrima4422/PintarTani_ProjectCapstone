5b33a3b4772f86843b147462e62dc3326b8c480a
iqrima4422 <iqrimannisa@gmail.com>
2023-06-17 11:22:40 +0700

    Mengubah dropdown Database Toko pada Sidebar

--- resources/views/AdminView/navbar.blade.php
+++ resources/views/AdminView/navbar.blade.php
@@ -12,17 +12,10 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
-
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
-        <!-- <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li> -->
     </ul>
 </nav>
